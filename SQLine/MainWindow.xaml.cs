8ce33c38f9de4c058778e6c26cfb958011faa960
PieceOfEon <danilskripka1996@gmail.com>
2023-02-05 14:57:36 +0200

    Add Read

--- SQLine/MainWindow.xaml.cs
+++ SQLine/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,28 +54,45 @@ namespace SQLine
 
         private void Enter_Click(object sender, RoutedEventArgs e)
         {
-            if(NameProduct.Text!="" && CountProduct.Text!="" && PriceProduct.Text!="" )
+            try
             {
-                using (var connection = new SqliteConnection("Data Source = userdata.db"))
+                if (NameProduct.Text != "" && CountProduct.Text != "" && PriceProduct.Text != "")
                 {
-                    connection.Open();
+                    using (var connection = new SqliteConnection("Data Source = userdata.db"))
+                    {
+                        connection.Open();
 
-                    SqliteCommand command = new SqliteCommand(connectionString);
-                    command.Connection = connection;
-                    //"+NameProduct+" ,"+PriceProduct+", "+CountProduct+"
-                    //Доабвление значений
-                    command.CommandText = "INSERT INTO Product(Name,Price, Count) VALUES('" + NameProduct.Text + "' ," + PriceProduct.Text + ", " + CountProduct.Text + ")";
-                    //int number = command.ExecuteNonQuery();
-                    a += Convert.ToDouble(PriceProduct.Text) * Convert.ToDouble(CountProduct.Text);
-                    TotalPrice.Text = a.ToString();
-                    //Console.WriteLine("Taблиа создана");
-                    int number = command.ExecuteNonQuery();
-                    MessageBox.Show($"В таблицу Product добавлено объектов:{number}");
-                    //Console.WriteLine($"В таблицу Userrs добавлено объектов:{number}");
-                    //Console.WriteLine($"Обновленно объектов: {number}");
+                        SqliteCommand command = new SqliteCommand(connectionString);
+                        command.Connection = connection;
+                        //"+NameProduct+" ,"+PriceProduct+", "+CountProduct+"
+                        //Доабвление значений
+                        command.CommandText = "INSERT INTO Product(Name,Price, Count) VALUES('" + NameProduct.Text + "' ," + PriceProduct.Text + ", " + CountProduct.Text + ")";
+                        //int number = command.ExecuteNonQuery();
+                        a += Convert.ToDouble(PriceProduct.Text) * Convert.ToDouble(CountProduct.Text);
+                        //TotalPrice.Text = a.ToString();
+                        //Console.WriteLine("Taблиа создана");
+                        int number = command.ExecuteNonQuery();
+                        MessageBox.Show($"В таблицу Product добавлено объектов:{number}");
+                        //Console.WriteLine($"В таблицу Userrs добавлено объектов:{number}");
+                        //Console.WriteLine($"Обновленно объектов: {number}");
+                        SqliteDataReader sqlite_datareader;
+                        SqliteCommand sqlite_cmd;
+                        sqlite_cmd = connection.CreateCommand();
+                        sqlite_cmd.CommandText = "SELECT SUM(Price * Count) FROM Product";
+
+                        sqlite_datareader=sqlite_cmd.ExecuteReader();
+                        while(sqlite_datareader.Read())
+                        {
+                            TotalPrice.Text = sqlite_datareader.GetString(0);
+                        }
+                        //int number2 = command.ExecuteReader();
+                        //MessageBox.Show(sqlite_datareader.ToString());
+                    }
+                    //Console.Read();
                 }
-                //Console.Read();
             }
+            catch (Exception b) { Console.WriteLine(b.Message); };
+
 
 
         }
